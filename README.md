bc71b2b5c41a07780c1bb58fcdef1087c32e9a43
Jaco Nel <jaco.nel@konga.com>
2016-07-18 23:49:49 +0200

    Initial commit of stacksup.
    StacksUp is a very small command line tool used to determine whether the
    instances of specified stacks are up and running.

---
+++ README.md
@@ -0,0 +1,31 @@
+# StacksUp
+Small command line utility used to verify which of the instances in our stacks are up and running as expected.
+
+It reads a very basic config file containing an array of stack ids and then determines and prints out the state of the instances linked to these stacks.
+
+Below is an example of the config:
+
+```js
+{
+    "stack_ids": [
+        "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+        "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+        "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+    ]
+}
+```
+
+Below is an example of how to run the command:
+
+```
+node index.js ./stacksup.json
+```
+
+or if linked correctly:
+
+```
+stacksup ./stacksup.json
+```
+
+Below is an example of the output to be expected:
+![Example output from Stacksup](http://i.imgur.com/EkcVruP.png)
---
+++ index.js
@@ -0,0 +1,81 @@
+#! /usr/bin/env node
+"use strict";
+
+// requires
+let Table  = require('cli-table');
+let colors = require('colors');
+let fs     = require('fs');
+let AWS    = require('aws-sdk');
+
+// exit codes
+const EXIT_NO_CONF      = 1;
+const EXIT_INVALID_CONF = 2;
+const EXIT_AWS_ERROR    = 3;
+
+// opsworks only works with us-east-1
+let opsworks = new AWS.OpsWorks({
+    region: "us-east-1"
+});
+
+// ennsure we have a config file
+if (process.argv[2] == undefined) {
+    console.log('Please supply a config file.');
+    process.exit(EXIT_NO_CONF);
+}
+
+// check that it is a file, load the file, get the stack ids
+// and then check each instance in that stack
+fs.stat(process.argv[2], (err, stats) => {
+    if (err || !stats.isFile()) {
+        console.log('Invalid config file');
+        process.exit(EXIT_INVALID_CONF);
+    }
+
+    let config = require(process.argv[2]);
+    let promises = config.stack_ids.map((stackId) => {
+        return new Promise((resolve, reject) => {
+            let params = { StackId: stackId };
+
+            opsworks.describeInstances(params, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    });
+
+    Promise.all(promises)
+    .then(stacks => {
+        var table = new Table({
+            head: ['Instance', 'Status']
+        });
+
+        let count = 0;
+        stacks.forEach((stack) => {
+            stack.Instances.forEach((instance) => {
+                let status;
+                switch (instance.Status) {
+                    case 'online':
+                        status = instance.Status.green;
+                        break;
+
+                    case 'stopped':
+                    case 'error':
+                    default:
+                        status = instance.Status.red;
+                }
+                // table.push([instance.Hostname, status]);
+                table.push(['staging-node5-0' + count, status])
+                count = count + 1;
+            });
+        });
+
+        console.log(table.toString());
+    })
+    .catch((err) => {
+        console.log('Unable to retrieve stack statuses.')
+        process.exit(EXIT_AWS_ERROR);
+    });
+});
